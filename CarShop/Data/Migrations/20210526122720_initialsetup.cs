9afc757d17d2f6c15eba82e28b2ce0b2d33be4c3
Bogdan Andone <bogdan.andone11@gmail.com>
2021-05-26 15:27:41 +0300

    added migration initial setup

---
+++ CarShop/Data/Migrations/20210526122720_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿tusing Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarShop.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    details = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
