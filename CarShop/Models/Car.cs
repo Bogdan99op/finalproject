c7a1ff9109d1d547b93da2e8e1fb9d3e6e6bf16d
roberto9985 <79300687+roberto9985@users.noreply.github.com>
2021-06-01 10:31:42 +0300

    add user field to database

--- CarShop/Models/Car.cs
+++ CarShop/Models/Car.cs
@@ -10,6 +10,7 @@ namespace CarShop.Models
         public int id { get; set; }
         public  string brand { get; set; }
         public string model { get; set; }
+        public string user { get; set; }
         public int price { get; set; }
         public string details { get; set; }
 
